056e3f94c387bf9d78bd9ddbf0537d304cb3af27
Aruna <aruna.silva@consultant.volvo.com>
2024-05-22 15:11:34 +0200

    more on inheritance related changes

--- unit_2_OopInTypescript/inheritanceDemo.ts
+++ unit_2_OopInTypescript/inheritanceDemo.ts
@@ -1,24 +1,34 @@
-class Person {
-    firstName : string;
-    lastName : string;
+// Inheritance | polimophism| Encapsulation|
 
-    greet (){
-        console.log("Hi from Person");
+class Person {
+
+    constructor (private firstName : string, private lastName : string) { // Encapsulation in action, No class attribute declaration  and assignment inside constructor required in this way. This is possible as TS allowed only a songle constructor.
+
+    }
+
+    protected greet (){
+        console.log("Hi from Person, " + this.firstName + " "+ this.lastName);
     }
 }
 
 
 class Programmer extends Person {
     greet() {
-        console.log("Hi from Programmer");
+        console.log("Hi from Programmer ");
     }
 
     greetLikePerson () {
-        super.greet();
+        super.greet(); // possible as person's greet method id protected
     }
 }
 
-let personx = new Programmer();
+
+
+
+
+let personx = new Programmer("Aruna ", "Silva"); // polimophism in action
 personx.greetLikePerson();
+
+
 
 
